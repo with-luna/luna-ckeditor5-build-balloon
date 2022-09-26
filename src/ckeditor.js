f783ba33c3dc642837187a378c0359c91d872a25
Ali Abdul Wahid <aly.dihaw96@gmail.com>
2022-09-26 12:28:47 +0700

    update to latest ckeditor

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -1,9 +1,3 @@
-/**
- * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
- */
-
-// The editor creator to use.
 import BalloonEditorBase from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
@@ -12,6 +6,7 @@ import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import CKBox from '@ckeditor/ckeditor5-ckbox/src/ckbox';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
@@ -26,11 +21,14 @@ import List from '@ckeditor/ckeditor5-list/src/list';
 import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import PictureEditing from '@ckeditor/ckeditor5-image/src/pictureediting';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+import Mention from '@ckeditor/ckeditor5-mention/src/mention';
 
 export default class BalloonEditor extends BalloonEditorBase {}
 
@@ -42,7 +40,9 @@ BalloonEditor.builtinPlugins = [
 	Bold,
 	Italic,
 	BlockQuote,
+	CKBox,
 	CKFinder,
+	CloudServices,
 	EasyImage,
 	Heading,
 	Image,
@@ -56,10 +56,12 @@ BalloonEditor.builtinPlugins = [
 	MediaEmbed,
 	Paragraph,
 	PasteFromOffice,
+	PictureEditing,
 	Table,
 	TableToolbar,
 	TextTransformation,
 	Underline,
+	Mention,
 	Alignment
 ];
 
@@ -72,8 +74,6 @@ BalloonEditor.defaultConfig = {
 			'link',
 			'underline',
 			'alignment'
-			// 'bulletedList',
-			// 'numberedList'
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
